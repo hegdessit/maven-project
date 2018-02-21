fb8b5ed7c990ce4ce50ce5435df3f20aae585e8b
hegdessit <hegdessit@gmail.com>
2018-02-21 11:23:12 -0800

    Added Outbound property Content-Type

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 import org.mule.tck.junit4.rule.DynamicPort;
 
@@ -23,4 +24,14 @@ public class HelloMavenTest extends FunctionalTestCase {
         return files;
     }
 
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieveFlights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
+      System.out.println("\n\nTestcase-2 dynamic port ----------> " + myPort.getNumber() + "\n\n");
+    }
+
+    
+    
 }
